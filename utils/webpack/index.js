4c6def5a37842c1c08fa046f1eb49e9907eb28ca
duhaime <douglas.duhaime@gmail.com>
2017-09-21 07:54:34 -0400

    remove legacy plugins and utils

--- utils/webpack/index.js
+++
@@ -1,6 +0,0 @@
-require("../..//assets/posts/spenserian-networks/js/bar-chart.js");
-require("../..//assets/posts/spenserian-networks/js/correlation-matrix.js");
-require("../..//assets/posts/spenserian-networks/js/steam-chart.js");
-require("../..//assets/posts/spenserian-networks/js/multiple-foci-chart.js");
-require("../..//assets/posts/spenserian-networks/js/discrete-scatterplot.js");
-require("../..//assets/posts/spenserian-networks/spenserian-networks.css");
